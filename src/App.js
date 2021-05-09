c443fab805586a70f0ec71ea613a9e6ccf0dc3b4
Hammad Seehar <hammad96@outlook.com>
2021-05-08 21:18:52 -0400

    added react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import SearchBar from './SearchBar';
-const App = () => {
-    return (
-      <SearchBar />);
-  
-  
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {Container, Row, Col, Modal, ModalBody,  ModalFooter, Button,MenuItem} from 'react-bootstrap'
+
+function App(props) {
+
+
+
+  return (
+   <div></div>
+    
+  );
 }
 
 export default App;
